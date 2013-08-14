46ac5c48b549c060aa109d0b2d5bcdb06c84007e
Gilad Goldberg <giladgo@gmail.com>
2013-08-14 17:09:41 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StageHog::Application.config.secret_token = '81f71c542056822d0a69455f44a17df69f1979d9bd2af997ee2885866691bac6429223e6b39ed9441046c53a2e40783ddd3c11553a76914e2837b3cebf54e28b'
