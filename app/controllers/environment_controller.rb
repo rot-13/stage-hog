22c548badb76b463d9055d8b97bdd3a29ec4ad5f
Gilad Goldberg <giladgo@gmail.com>
2013-10-23 18:43:40 +0300

    Checking for authorization in use+free

--- app/controllers/environment_controller.rb
+++ app/controllers/environment_controller.rb
@@ -5,15 +5,16 @@ class EnvironmentController < ApplicationController
 
   def use
     @environment = Environment.find(params['id'])
-    @environment.using_user = self.current_user
-    @environment.save!
-
+    if current_user.authorized_for_project(@environment.project)
+      @environment.using_user = self.current_user
+      @environment.save!
+    end
     redirect_to project_index_url
   end
 
   def free
     @environment = Environment.find(params['id'])
-    if @environment.using_user.id == self.current_user.id
+    if @environment.using_user.id == self.current_user.id and current_user.authorized_for_project(@environment.project)
       @environment.using_user = nil
       @environment.save!
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ActiveRecord::Base
   has_many :environments_using, foreign_key: 'in_use_by', class_name: 'Environment'
   has_many :project_auths
   has_many :authorized_projects, source: :project, through: :project_auths
+
+
+  def authorized_for_project(project)
+    authorized_projects.map(&:id).include?(project.id)
+  end
 end
